70ca669de1f971c82d3db7f46a33d3dfc5f2d687
Mathias Unger <matiunger@gmail.com>
2021-06-07 19:44:33 -0300

    react setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>matiunger.com</h2>
+      <p>coming soon</p>
+    </div>
+  );
+}
+
+export default App;
