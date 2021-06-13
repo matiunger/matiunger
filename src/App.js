82a46ce21a6fc3a1ea95ff11642fcb2907500cbe
Mathias Unger <matiunger@gmail.com>
2021-06-13 16:39:56 -0300

    navigation and routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,58 @@
-import logo from './logo.svg';
+import React from 'react';
+import logo from './img/web_avatar2.png';
 import './App.css';
+import { Switch, Route, Link, useLocation } from "react-router-dom";
+import { animated, useTransition } from "react-spring";
+
+
+function Header () {
+  return (
+    <header>
+      <div className="logo-container"><img className="logo" src={logo} alt="Logo" /></div>
+      <nav>
+      <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/projects">Projects</Link>
+          </li>
+          <li>
+            <Link to="/art">Art
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+
+}
+
 
 function App() {
+
+  const location = useLocation();
+
+  const transitions = useTransition(location, {
+    from: { opacity: 0, width: "0%" },
+    enter: { opacity: 1, width: "100%" },
+    leave: { opacity: 0, width: "0%" }
+  });
+
   return (
     <div className="App">
-      <h2>matiunger.com</h2>
-      <p>coming soon</p>
+      <Header />
+      <div className="Routes">
+        {transitions((props, item) => (
+          <animated.div style={props}>
+            <Switch location={item}>
+              <Route exact path="/"  >home</Route>
+              <Route exact path="/projects">projects</Route>
+              <Route exact path="/art">art</Route>
+            </Switch>
+          </animated.div>
+        ))}
+      </div>
     </div>
   );
 }
