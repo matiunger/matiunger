2b791914874d9405a494fcf33fa03f047396d3da
Mathias Unger <matiunger@gmail.com>
2021-06-21 11:32:51 -0300

    new build

---
+++ docs/precache-manifest.68afb72fff4f85c46f0ed790ac4e9989.js
@@ -0,0 +1,222 @@
+self.__precacheManifest = [
+  {
+    "revision": "a52a647d943f640ad461",
+    "url": "/static/css/main.288098f1.chunk.css"
+  },
+  {
+    "revision": "a52a647d943f640ad461",
+    "url": "/static/js/main.5771cc4d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b606182e730f246959a2",
+    "url": "/static/js/2.7927555f.chunk.js"
+  },
+  {
+    "revision": "1744fb2fb253e8be795ec7e77753bbbe",
+    "url": "/static/media/web_avatar2.1744fb2f.png"
+  },
+  {
+    "revision": "bc5381bd7dd8264a6c5f10ec59ee6e71",
+    "url": "/static/media/Amarnecer.bc5381bd.jpg"
+  },
+  {
+    "revision": "c347e4707f9d3a75143cfcc7d20ce5c5",
+    "url": "/static/media/Calm.c347e470.jpg"
+  },
+  {
+    "revision": "f975044ded881f7957c7590cb2d85cbb",
+    "url": "/static/media/Crazy_eyes.f975044d.jpg"
+  },
+  {
+    "revision": "9a4fe8dd11daf27d2900fac09ea7eb7a",
+    "url": "/static/media/Flowers.9a4fe8dd.jpg"
+  },
+  {
+    "revision": "5d7edda0e09f30fc704fdaf2f4e8b838",
+    "url": "/static/media/Fruits.5d7edda0.jpg"
+  },
+  {
+    "revision": "8e032a790de2e8ee1c913c4e22b0ee4a",
+    "url": "/static/media/Glaciar.8e032a79.jpg"
+  },
+  {
+    "revision": "ace71456024e07ec714f614885b2120e",
+    "url": "/static/media/Ice.ace71456.jpg"
+  },
+  {
+    "revision": "11537f03316f15fef6af33363d522c94",
+    "url": "/static/media/Papagayo.11537f03.jpg"
+  },
+  {
+    "revision": "e69b50fca14dd132e88747274e8fe176",
+    "url": "/static/media/Playa.e69b50fc.jpg"
+  },
+  {
+    "revision": "8146afa3716e4f8c508f73852388473a",
+    "url": "/static/media/Purmamarca.8146afa3.jpg"
+  },
+  {
+    "revision": "d918f77c1b7c546a783dc714333a5b15",
+    "url": "/static/media/Sea.d918f77c.jpg"
+  },
+  {
+    "revision": "ea036faecb4b22433e6293e8022b7512",
+    "url": "/static/media/Tucan.ea036fae.jpg"
+  },
+  {
+    "revision": "ba246e08ca74e906e239b73c3ff5a359",
+    "url": "/static/media/caballo.ba246e08.png"
+  },
+  {
+    "revision": "0991c0a37120b102b61f6dc457196e5a",
+    "url": "/static/media/camaleon.0991c0a3.png"
+  },
+  {
+    "revision": "ecc63840b5511ddb656d79208f5cebf9",
+    "url": "/static/media/peritomoreno.ecc63840.png"
+  },
+  {
+    "revision": "299809f7e92e3e29d49b279bd3904e90",
+    "url": "/static/media/leon.299809f7.png"
+  },
+  {
+    "revision": "91d725cc65493df6ba745846f50dedf3",
+    "url": "/static/media/olascalm2.91d725cc.png"
+  },
+  {
+    "revision": "74622c65d8fa224e9ada0ab70e81a1a1",
+    "url": "/static/media/selva.74622c65.png"
+  },
+  {
+    "revision": "5d9b4a58831c0b58b69acc9237735851",
+    "url": "/static/media/tigre.5d9b4a58.png"
+  },
+  {
+    "revision": "1f7c1a688096981f3a5129d146f4487c",
+    "url": "/static/media/wave.1f7c1a68.png"
+  },
+  {
+    "revision": "6a628fbae853cd83451d74eff9da2c22",
+    "url": "/static/media/Ale.6a628fba.jpg"
+  },
+  {
+    "revision": "2d1603d07b1e39f9f83e74d75fa2f4ca",
+    "url": "/static/media/Alex.2d1603d0.jpg"
+  },
+  {
+    "revision": "81ea8ceb30105c7f22ec7ba7601ec126",
+    "url": "/static/media/Angel_Cappa.81ea8ceb.jpg"
+  },
+  {
+    "revision": "56c62decc124805eabe675c0a8e491ff",
+    "url": "/static/media/Falcioni.56c62dec.jpg"
+  },
+  {
+    "revision": "3896c7f6a97c8fca44602ca67da25fcd",
+    "url": "/static/media/Federer.3896c7f6.jpg"
+  },
+  {
+    "revision": "4115893f2ffcc1587cbdc73af9d53757",
+    "url": "/static/media/Hangling.4115893f.jpg"
+  },
+  {
+    "revision": "999acd007028f2ab3d658f66f60ab349",
+    "url": "/static/media/Iggy_Pop.999acd00.jpg"
+  },
+  {
+    "revision": "4ca62fd8fb294cfa73ac4b60e204bb21",
+    "url": "/static/media/Ilse.4ca62fd8.jpg"
+  },
+  {
+    "revision": "79197c1313772909cd696a3bc2c9904c",
+    "url": "/static/media/Lanata.79197c13.jpg"
+  },
+  {
+    "revision": "e60ba08dc7ce1e7e765510a6f845a711",
+    "url": "/static/media/Maradona.e60ba08d.jpg"
+  },
+  {
+    "revision": "d33db0ff0161dd98d053537d5e0da66d",
+    "url": "/static/media/Sonia.d33db0ff.jpg"
+  },
+  {
+    "revision": "8085b36ef8433b748cbbc409af215a69",
+    "url": "/static/media/Steve_Jobs.8085b36e.jpg"
+  },
+  {
+    "revision": "af2a54eccea40d87412935935d1e0daf",
+    "url": "/static/media/bungalow1.af2a54ec.jpg"
+  },
+  {
+    "revision": "5e44edb9eb73c6b4e83f9f4220b06d75",
+    "url": "/static/media/bungalow2.5e44edb9.jpg"
+  },
+  {
+    "revision": "90596830a1aaccdc7a084c3e3bee1974",
+    "url": "/static/media/bungalow3.90596830.jpg"
+  },
+  {
+    "revision": "2ad8d9f2f70e21a31e3c6ad97d58828e",
+    "url": "/static/media/bungalow4.2ad8d9f2.jpg"
+  },
+  {
+    "revision": "c36f702d5f4f3da3675671bd20e7afc7",
+    "url": "/static/media/bungalow5.c36f702d.jpg"
+  },
+  {
+    "revision": "67e0f6a814bc3ca1453ac529ef569039",
+    "url": "/static/media/bungalow6.67e0f6a8.jpg"
+  },
+  {
+    "revision": "e5572776431528ad0d9f94ef0ee4322f",
+    "url": "/static/media/bungalow7.e5572776.jpg"
+  },
+  {
+    "revision": "5ad786512ed0d94174c537a171add5d1",
+    "url": "/static/media/bungalow8.5ad78651.jpg"
+  },
+  {
+    "revision": "0c9c934f6d83e3e422dd5ca2b5a2fa2b",
+    "url": "/static/media/tubirra1.0c9c934f.png"
+  },
+  {
+    "revision": "bc9bed62adaf79386fbc75591e3b8411",
+    "url": "/static/media/tubirra2.bc9bed62.png"
+  },
+  {
+    "revision": "f0d8d7aa0e5e897dbb348e7e1de6a9ac",
+    "url": "/static/media/tubirra3.f0d8d7aa.png"
+  },
+  {
+    "revision": "e00953defc33b0ed3fdc131553691a34",
+    "url": "/static/media/tubirra4.e00953de.png"
+  },
+  {
+    "revision": "da7370f3db8af8a8e6556ca2455209f6",
+    "url": "/static/media/tubirra5.da7370f3.png"
+  },
+  {
+    "revision": "0c95848a92e990545235727311fc430a",
+    "url": "/static/media/screenshot1.0c95848a.jpeg"
+  },
+  {
+    "revision": "84fd25c2914d5ee0e08c32d28b9931c2",
+    "url": "/static/media/screenshot2.84fd25c2.jpeg"
+  },
+  {
+    "revision": "1da7a321e4b4062048953925ebd4347e",
+    "url": "/static/media/screenshot3.1da7a321.jpeg"
+  },
+  {
+    "revision": "617dff16e05e72e2284569a7355fc8d5",
+    "url": "/static/media/months_columns.617dff16.png"
+  },
+  {
+    "revision": "96a735ab1af65e335a2f2babad584397",
+    "url": "/index.html"
+  }
+];
